c764242ee0234290728e3dd6c6bc3e9319f3aa35
Sreyas <sreyas.codaw@gmail.com>
2025-04-20 22:32:55 +0530

    added

---
+++ website/contact.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!-- ===================================================
+         Metadata & Document Setup
+         =================================================== -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Meta description for SEO -->
+	<meta name="description" content="XXXXX">
+
+	<!-- Document Title -->
+	<title>Contact Us</title>
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="assets/logo.ico">
+
+	<!-- ===================================================
+         Fonts & Stylesheets
+         =================================================== -->
+
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+	<!-- Custom Styles -->
+	<link href="styles/global.css" rel="stylesheet">
+	<link href="styles/contact.css" rel="stylesheet">
+
+	<!-- ===================================================
+         Scripts
+         =================================================== -->
+	<!-- <script src="scripts/XXXXXXXX.js" defer></script> -->
+</head>
+
+<body>
+	<!-- ===================================================
+         Navigation Section
+         - Included via PHP to allow reusability across pages.
+         =================================================== -->
+	<?php include("nav.php"); ?>
+
+	
+
+	<!-- ===================================================
+         Footer Section
+         - Included via PHP for consistency across pages.
+         =================================================== -->
+	<?php include("footer.html"); ?>
+
+</body>
+
+</html>
