fb8df53a69d80b7fbf9559d719b5e98b3b6c87be
Sreyas <sreyas.codaw@gmail.com>
2025-05-01 19:24:43 +0530

    removed comments

--- website/scripts/footer.js
+++ website/scripts/footer.js
@@ -21,29 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', updatePosition);
     updatePosition();
 
-//     let firstClick = false;
-
-// whatsapp.addEventListener('click', function (e) {
-//     const isMobile = window.innerWidth <= 768;
-
-//     if (isMobile && !firstClick) {
-//         e.preventDefault(); // stop link
-//         whatsapp.classList.add('expanded'); // optional for styling
-//         const text = whatsapp.querySelector('.whatsapp-text');
-//         text.style.opacity = '1';
-//         text.style.maxWidth = '200px';
-//         text.style.paddingRight = '1em';
-//         firstClick = true;
-
-//         // Optional: reset after some time if user doesn't click again
-//         setTimeout(() => {
-//             firstClick = false;
-//             text.style.opacity = '0';
-//             text.style.maxWidth = '0';
-//             text.style.paddingRight = '0';
-//             whatsapp.classList.remove('expanded');
-//         }, 5000);
-//     }
-// });
-
 });
